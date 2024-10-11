fb006e8c183810df604eb0814d326d0ea4d01f68
Gaël <HadriaxDev@protonmail.com>
2024-10-11 15:20:42 +0200

    Add scripts folder

---
+++ Scripts/SceneNavigation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Quentin
+{
+    public class SceneNavigation
+    {
+        public static void LobbyMulti()
+        {
+            SceneManager.LoadScene("Production/Scenes/Menu/Lobby");
+        }
+
+        public static void MainMenu()
+        {
+            SceneManager.LoadScene("Production/Scenes/Menu/MainMenu");
+        }
+
+        public static void VehiculeSelection()
+        {
+            SceneManager.LoadScene("Production/Scenes/Customization/MenuSelectionVehicules");
+        }
+
+        internal static void Customization()
+        {
+            SceneManager.LoadScene("Production/Scenes/Customization/Garage");
+        }
+
+        internal static void RaceMulti()
+        {
+            SceneManager.LoadScene("Production/Scenes/Races/RaceMulti");
+        }
+
+        internal static void RaceSolo()
+        {
+            SceneManager.LoadScene("Production/Scenes/Races/RaceSolo");
+        }
+
+        internal static void AIRace()
+        {
+            SceneManager.LoadScene("Production/Scenes/Races/AIRace");
+        }
+
+        internal static void TestInput()
+        {
+            SceneManager.LoadScene("Development/TestRewired/SampleScene");
+        }
+    }
+}
